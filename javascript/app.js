0ce78001028efdb8def9470e16b7a6a3a42f472e
CeeDubyuh <clwomack2000@gmail.com>
2021-10-22 16:13:07 -0700

    Cleanup/JS Addition
    little more JavaScript for service workers/ fetch events and some cleanup in index.html

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     //Register sw
     navigator.serviceWorker.register('/javascript/serviceworker.js')
-    .then((reg) => console.log('service worker registered', reg))
-    .catch((err) => console.log('service worker not registered', err))
+    .then((reg) => console.log('sw registered!', reg))
+    .catch((err) => console.log('sw not registered!', err))
 }
