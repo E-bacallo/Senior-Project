8ef5f4d12cecce7472807715402589789ab0cb3c
CeeDubyuh <clwomack2000@gmail.com>
2021-10-27 10:58:00 -0700

    Dynamic Caching
    Dynamic page caching (should) be working as intended.

--- pages/serviceworker.js
+++ pages/serviceworker.js
@@ -1,4 +1,5 @@
-const openCache = 'static-cache';
+const openCache = 'static-cache-v1';
+const dynamicCache = 'dynamic-cache-v1';
 //Const to store page assets ready to cache.
 const pageAssets = [
 '/', 
@@ -28,14 +29,27 @@ self.addEventListener('install', evt => {
 
 //Activate the Service Worker
 self.addEventListener('activate', evt => {
+    //On Activate, delete old cache and install new one.
+    evt.waitUntil(
+        caches.keys().then(keys =>{
+            return Promise.all(keys.filter(key => key != openCache)
+            .map(key => caches.delete())
+            )
+        })
+    );
 });
 
 //Look in the cache, and match requests to the assets.
 self.addEventListener('fetch', evt =>{
     evt.respondWith(
         caches.match(evt.request).then(cacheResponse => {
-            return cacheResponse || fetch(evt.request);
             //Return the matching asset, or repeat the request.
+            return cacheResponse || fetch(evt.request).then(fetchResponse => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                })
+            });   
         })
     );
 });
